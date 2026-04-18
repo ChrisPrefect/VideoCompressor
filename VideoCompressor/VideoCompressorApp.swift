1f6d2021fdc71c496d34f2c5c5417964ef3fcc22
Chris <info@iteconomy.ch>
2026-04-19 00:59:14 +0200

    Initial Commit

---
+++ VideoCompressor/VideoCompressorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCompressorApp.swift
+//  VideoCompressor
+//
+//  Created by Chris on 19.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct VideoCompressorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
